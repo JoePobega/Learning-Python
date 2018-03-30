628a20297d90bff7b269590840f57e3b9f586842
Joseph Pobega <37122818+JoePobega@users.noreply.github.com>
2018-03-30 14:07:12 -0400

    Create lesson2.py

---
+++ lesson2/lesson2.py
@@ -0,0 +1,38 @@
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+#Mistake on purpose
+message = "Hello Python Crash Course reader!"
+print(mesage)
+
+mesage = "Hello Python Crash Course reader!"
+print(mesage)
+
+
+#Strings
+
+"This is a string."
+'this is also a string.'
+
+#Changing case in a string with methods
+
+name = "ada.lovelace"
+print(name.title())
+
+#Adding whitespace to strings with tabs or newlines
+
+print("Python")
+
+print("\tPython")
+
+#Stripping whitespace
+
+favorite_language = 'python'
+favorite_language
+
+favorite_language.rstrip()
+
+favorite_language
