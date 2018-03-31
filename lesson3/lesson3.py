1635f95c7960b0edaacdef8cb26200acfc285bde
Joseph Pobega <37122818+JoePobega@users.noreply.github.com>
2018-03-31 11:33:22 -0400

    Create lesson3.py
    this is lesson3 of learning python. it introduces lists.

---
+++ lesson3/lesson3.py
@@ -0,0 +1,27 @@
+'lesson3' 'introducing lists'
+#!usr/bin/env python
+
+#The zero tells python wha bicycle to print out in the order.
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0])
+
+
+#This is another example of a certain name in the catagory bicycles to be printed out specificaly.
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[1])
+print(bicycles([3])
+
+
+#The numbers in the message indicate what needs to be printed out in the order of the bicycles as said in the message.
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+message = "My first bicycle was  a " + bicycles[0].title() + "."
+
+print(message)
+
+
+#Removing an element from a list
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+del motorcycles[0]
+print (motorcycles)
