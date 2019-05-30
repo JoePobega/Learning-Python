9f9017fd68af8f83fb8e6e2df1a8a31775b55f34
Joseph Pobega <37122818+JoePobega@users.noreply.github.com>
2019-05-29 20:12:58 -0400

    lesson4.py
    if and else statement.

---
+++ lesson4.py
@@ -0,0 +1,24 @@
+#The code below is an example to an else/if statement.
+
+
+#If statement
+
+x = 5
+y = 10
+if y > 10:
+    print("That is correct!")
+
+
+
+
+
+
+x = 5
+y = 10
+if x > y:
+    print("that is correct!")
+else:
+   print("that is NOT correct!")
+   
+   
+   
